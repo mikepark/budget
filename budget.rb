14f533e5d3ce15b788e667cbc30e9c2ac433f4c8
Mike Park <MikePark.rb@GMail.com>
2011-01-22 16:24:26 -0500

    core budget functionality from local repo

---
+++ budget.rb
@@ -0,0 +1,55 @@
+
+require 'date'
+
+class Bill
+  def initialize(day, amount, desc)
+    @day = day
+    @amount = amount
+    @desc = desc
+  end
+  def today(date)
+    puts "#{@desc} #{@amount}" if (date.mday==@day)
+    ((date.mday==@day) ? @amount : 0)
+  end
+end
+
+class Budget
+  attr_accessor :balance, :events
+  def initialize(initial_balance)
+    @balance = initial_balance
+    @events = []
+  end
+  def day_of_month(day, amount, desc)
+    @events << Bill.new(day, amount, desc)
+  end
+  def payday(date, amount)
+    @first_payday = date
+    @pay = amount
+  end
+  def future(days)
+    File.open('budget.m','w') do |f|
+      f.puts "#!/usr/bin/env octave\ndb=["
+      date = Date.today
+      balance = @balance
+      days.times do |day|
+        last_balance = balance
+        @events.each do |event|
+          balance += event.today(date)
+        end
+
+        delta = date.yday - @first_payday.yday
+        if ( 0 == delta.remainder(14) ) 
+          balance += @pay 
+          puts "pay #{@pay}"
+        end
+        if (last_balance != balance)
+          puts [date.to_s, balance.to_s].join(' : ')
+          f.puts [365*(date.year-2000)+date.yday, balance.to_s].join(' , ')+" % oct"        
+        end
+        date = date.next
+      end
+      f.puts "];\nplot(db(:,1),db(:,2))\nprint -deps budget.eps"
+    end
+    `octave budget.m`
+  end
+end
