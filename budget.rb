85bbdf69fae3be71ee1fbb075a4733314106f415
Mike Park <Mike.Park@NASA.gov>
2016-07-02 20:02:22 -0400

    to tecplot and allow multiple biweeklys

--- budget.rb
+++ budget.rb
@@ -13,52 +13,63 @@ class Bill
   end
 end
 
+class Payday
+  def initialize(first_date, amount, desc)
+    @first_date = first_date
+    @amount = amount
+    @desc = desc
+  end
+  def today(date)
+    delta = date.mjd - @first_date.mjd
+    puts "#{@desc} #{@amount}" if (0 == delta.remainder(14))
+    ((0 == delta.remainder(14)) ? @amount : 0)
+  end
+end
+
 class Budget
   attr_accessor :balance, :events
   def initialize(initial_balance)
     @balance = initial_balance
     @events = []
+    @paydays = []
   end
   def day_of_month(day, amount, desc)
     @events << Bill.new(day, amount, desc)
   end
-  def payday(date, amount)
-    @first_payday = date
-    @pay = amount
+  def biweekly_from(date, amount, desc)
+    @paydays << Payday.new(date, amount, desc)
   end
   def future(days)
     File.open('budget.dat','w') do |tec|
-    File.open('budget.m','w') do |f|
-      f.puts "#!/usr/bin/env octave"
-      f.puts "db=["
-      date = Date.today
-      balance = @balance
-      days.times do |day|
-        last_balance = balance
-        @events.each do |event|
-          balance += event.today(date)
-        end
-
-        if ( @first_payday ) then
-          delta = date.yday - @first_payday.yday
-          if ( 0 == delta.remainder(14) ) 
-            balance += @pay 
-            puts "pay #{@pay}"
+      File.open('budget.m','w') do |f|
+        f.puts "#!/usr/bin/env octave"
+        f.puts "db=["
+        date = Date.today
+        balance = @balance
+        days.times do |day|
+          last_balance = balance
+          
+          @events.each do |event|
+            balance += event.today(date)
           end
+          
+          @paydays.each do |payday|
+            balance += payday.today(date)
+          end
+          
+          if (last_balance != balance)
+            puts [date.to_s, balance.to_s].join(' : ')
+            f.puts [date.jd-Date.today.jd, balance.to_s].join(' , ')+" % oct"        
+            tec.puts [date.jd-Date.today.jd, balance.to_s].join(' ')
+          end
+          date = date.next
         end
-        if (last_balance != balance)
-          puts [date.to_s, balance.to_s].join(' : ')
-          f.puts [date.jd-Date.today.jd, balance.to_s].join(' , ')+" % oct"        
-          tec.puts [date.jd-Date.today.jd, balance.to_s].join(' ')
-        end
-        date = date.next
+        f.puts "];"
+        f.puts "plot(db(:,1),db(:,2))"
+        f.puts "print -deps budget.eps"
+        f.puts "system('epstopdf budget.eps')"      
       end
-      f.puts "];"
-      f.puts "plot(db(:,1),db(:,2))"
-      f.puts "print -deps budget.eps"
-      f.puts "system('epstopdf budget.eps')"      
     end
-    end
-    `octave -q budget.m`
+    #    `octave -q budget.m`
   end
 end
